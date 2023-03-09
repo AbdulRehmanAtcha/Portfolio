a90cdaf91296d1c1702fbc53e878e2860c699d0c
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2023-03-09 14:57:37 +0500

    Complete

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
@@ -5,28 +6,51 @@ import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Project from "./components/projects/Project";
 import Skills from "./components/skills/Skills";
+import GridLoader from "react-spinners/GridLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+  }, []);
+  useEffect(() => {
+    window.onload = () => {
+      setLoading(false);
+    };
+  }, []);
   return (
     <div>
-      <Navbar />
-      <br />
-      <br id="Home"/>
-      <br />
-      <Home />
-      <br id="About" />
-      <br />
-      <About />
-      <br id="Skills" />
-      <br />
-      <Skills />
-      <br id="Projects" />
-      <br />
-      <br />
-      <br />
-      <Project />
-      <br id="Contact"/>
-      <Contact />
+      {loading ? (
+        <div className="main-loader">
+          <GridLoader
+            color="#64ffda"
+            size={20}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <br />
+          <br id="Home" />
+          <br />
+          <Home />
+          <br id="About" />
+          <br />
+          <About />
+          <br id="Skills" />
+          <br />
+          <Skills />
+          <br id="Projects" />
+          <br />
+          <br />
+          <br />
+          <Project />
+          <br id="Contact" />
+          <Contact />
+        </>
+      )}
     </div>
   );
 }
