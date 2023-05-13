e96802a47cfbbeae2d5d470709cd0523df0b2288
AbdulRehmanAtcha <abdulrehman1239091@gmail.com>
2023-05-13 13:07:32 +0500

    Some Changes Done

--- web/src/components/about/About.jsx
+++ web/src/components/about/About.jsx
@@ -13,20 +13,20 @@ const About = () => {
       {/* <br />
       <br /> */}
       <div className="about-head">
-        <h2 ><span>02 </span>About Me</h2>
+        <h2>
+          <span>02 </span>About Me
+        </h2>
       </div>
       <div className="about-main-box">
         <div className="about-left"></div>
         <div className="about-right">
           <h2>
-            Hey, my name is Abdul Rehman. I am 20 years old and from Karachi,
-            Pakistan. I am currently studying at the University Of Karachi in
-            the field of Software Engineering(BSSE). Since starting in Software
-            Engineering, my interest in web development has increased. I started
-            HTML and CSS(Basics) on Youtube, but after that, I joined Saylani
-            Mass IT Training Program(SMIT) where I learned Web Development in
-            detail, more specifically, MERN Stack development. I am open for
-            work.
+            My name is Abdul Rehman. Currently student of Bachelor of Science in
+            Software Engineering. <br /> A highly motivated software engineer
+            student passionate about ReactJs and related technologies. Sound
+            knowledge of HTML, CSS, JavaScript, and Bootstrap for creating
+            visually appealing and responsive web interfaces. Also good
+            knowledge of ExpressJs, MongoDB, and JWT. <br /> Aim to become a best MERN STACK DEVELOPER.
           </h2>
         </div>
       </div>
